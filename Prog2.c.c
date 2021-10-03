5df08cb2f85115be54dc9aed3c6c84fcb1a89936
VishalChaturvedi6102 <91264004+VishalChaturvedi6102@users.noreply.github.com>
2021-10-03 15:22:12 +0530

    Add files via upload

---
+++ Prog2.c.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{   int n;
+    printf("Enter The Integer /n");
+    scanf("%d",&n);
+    printf("The Enter Integer Is : %d",n);
+}
---
+++ Prog3.c
@@ -0,0 +1,8 @@
+// to add two integers
+#include <stdio.h>
+int main()
+{  int a=10 , b=20 , sum;
+sum=a+b;
+printf("The addition of two integers is  :  %d",sum);
+return 0;
+   }
