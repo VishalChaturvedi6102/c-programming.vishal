1a2ab03d5ddc4580961cb743002be174e67c79dc
VishalChaturvedi6102 <91264004+VishalChaturvedi6102@users.noreply.github.com>
2021-10-06 22:42:05 +0530

    Add files via upload

---
+++ Prog37.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() 
+{
+  float input;
+  printf("Enter a floating point number :");
+  scanf("%f", &input);
+  printf("The entered number in Exponential form '%e'\n", input);
+
+  return 0;
+}
+    
