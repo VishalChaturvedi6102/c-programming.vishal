c6a64e04595aaf4417071e734ed4b90f70eebb97
VishalChaturvedi6102 <91264004+VishalChaturvedi6102@users.noreply.github.com>
2021-10-14 23:29:32 +0530

    Update Prog28.c

--- Prog28.c
+++ Prog28.c
@@ -1,20 +1,13 @@
 // C-program to calculate area of an equilateral triangle .
 #include <stdio.h>
-#include <math.h> // Used for sqrt() function
+#include <math.h>
 
 int main()
 {
-    float side, area;
-
-    /* Input side of equilateral triangle */
+    float s, area;
     printf("Enter side of an equilateral triangle: ");
-    scanf("%f", &side);
-
-    /* Calculate area of equilateral triangle */
-    area = (sqrt(3) / 4) * (side * side);
-
-    /* Print resultant area */
-    printf("Area of equilateral triangle = %.2f sq. units", area);
-  
+    scanf("%f", &s);
+    area = (sqrt(3) / 4) * (s * s);
+    printf("Area of equilateral triangle = %.2f", area);
   return 0 ; 
 }
