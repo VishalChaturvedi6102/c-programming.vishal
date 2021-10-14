2374ae623079705dcf13fda2977f5252256ccd15
VishalChaturvedi6102 <91264004+VishalChaturvedi6102@users.noreply.github.com>
2021-10-14 23:19:17 +0530

    Update Prog11.c

--- Prog11.c
+++ Prog11.c
@@ -1,20 +1,15 @@
 // C Program to calculate area of a triangle using heron's formula .
 #include <stdio.h>
 #include <math.h>
-#include <conio.h>
- 
 int main()
 {
-    float sideOne, sideTwo, sideThree, s, area;
+    float side1, side2, side3, s, area;
     printf("Enter the length of three sides of triangle\n");
-    scanf("%f %f %f", &sideOne, &sideTwo, &sideThree);
-    /* Area of any triangle = 
-       sqrt(s*(s-sideOne)*(s-sideTwo)*(s-sideThree))
-       Where s = (sideOne + sideTwo + sideThree)/2  */
-    s = (sideOne + sideTwo + sideThree)/2;
-    area = sqrt(s*(s-sideOne)*(s-sideTwo)*(s-sideThree));
-    printf("Area of triangle : %0.4f\n", area);
-     
-    getch();
+    scanf("%f %f %f", &side1, &side2, &side3);
+    
+    s = (side1 + side2 + side3)/2;
+    area = sqrt(s*(s-side1)*(s-side2)*(s-side3));
+
+    printf("Area of triangle : %0.2f\n", area);
     return 0;
 }
